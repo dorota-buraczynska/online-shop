0bc13a5f24f28238069785c0bb8a252e4571585e
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-05-30 15:48:04 +0200

    init

---
+++ script/script.js
@@ -0,0 +1,61 @@
+//nav
+$('.nav__button').on('click', function (event) {
+    var buttonActiveClass = 'nav__button--active';
+    $(this).toggleClass(buttonActiveClass);
+    $('.nav__menu').toggleClass('visible');
+});
+
+$('.nav__menu-item').on('click', function (event) {
+    event.preventDefault();
+    var listItemActiveClass = 'nav__menu-item--active';
+    $(this).siblings().removeClass(listItemActiveClass);
+    $(this).addClass(listItemActiveClass);
+});
+
+//slider
+var changeActiveDot = function (index) {
+    $('.slider__dot')
+        .eq(index)
+        .addClass('slider__dot--active')
+        .siblings().removeClass('slider__dot--active');
+};
+
+var changeActivePhoto = function (index) {
+    $('.slider__slide')
+        .eq(index)
+        .addClass('slider__slide--active')
+        .siblings().removeClass('slider__slide--active');
+};
+
+var getCurrentPhotoIndex = function () {
+    return $('.slider__slide.slider__slide--active').index();
+};
+
+var onDotClick = function (event) {
+    clearInterval(autoSlide);
+    changeActiveDot($(this).index());
+    changeActivePhoto($(this).index());
+    currentIndex = getCurrentPhotoIndex();
+    startSlideshow();
+};
+
+$('.slider__dot').on('click', onDotClick);
+
+var autoSlide;
+var currentIndex = 0;
+var photos = $('.slider__slide');
+
+var startSlideshow = function () {
+    autoSlide = setInterval(function () {
+        currentIndex++;
+        if (currentIndex > photos.length - 1) {
+            currentIndex = 0;
+        }
+        // console.log(currentIndex);
+        changeActivePhoto(currentIndex);
+        changeActiveDot(currentIndex);
+
+    }, 5000);
+};
+
+startSlideshow();
