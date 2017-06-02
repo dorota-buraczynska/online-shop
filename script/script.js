0e2599cd4afeb9af01267d7a210c21999ba154be
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-06-02 14:34:37 +0200

    add prev and next button to preview modal box, and fuunctionality for them

--- script/script.js
+++ script/script.js
@@ -200,7 +200,7 @@ $('.products__button-next').on('click', function () {
 
 //back to top
 $('.products__button-top').on('click', function () {
-   $('html, body').animate({scrollTop: 0}, 1500);
+    $('html, body').animate({scrollTop: 0}, 1500);
 });
 
 //add products to small basket, modal-box
@@ -212,7 +212,7 @@ $('.products__button').on('click', function () {
 });
 
 $('.modal-box__shopping-button, .modal-box__close-button').on('click', function () {
-   $('.modal-box').hide();
+    $('.modal-box').hide();
 });
 
 //preview
@@ -223,20 +223,68 @@ $('.products__preview').on('click', function () {
     var productPrice = $(this).closest('.products__item').data('price');
     var productTitle = $(this).siblings('.products__title').text();
     var productDescription = $(this).siblings('.products__description').text();
+    var photoNumber = $(this).closest('.products__item').index();
 
-    console.log(productTitle);
     $('.preview').show();
-    $('.preview__photo').attr('src', photoSrc);
+    $('.preview__photo-item').attr('src', photoSrc);
     $('.preview__product-fabric span').text(productFabric);
     $('.preview__product-size span').text(productSize);
     $('.preview__product-price span').text(productPrice + '$');
     $('.preview__product-title').text(productTitle);
     $('.preview__product-description-text').text(productDescription);
+    $('.preview__content').data('product-number', photoNumber);
 });
 
 $('.preview__close-button, .preview__shopping-button').on('click', function () {
-   $('.preview').hide();
+    $('.preview').hide();
 });
 
+var showNextPreview = function (element) {
+    var numberOfNextProduct = $(element).closest('.preview__content').data('product-number');
+    var nextPhotoSrc = $('.products__item').eq(numberOfNextProduct + 1).find('.products__photo').attr('src');
+    var nextProductFabric = $('.products__item').eq(numberOfNextProduct + 1).data('fabric');
+    var nextProductSize = $('.products__item').eq(numberOfNextProduct + 1).data('size');
+    var nextProductPrice = $('.products__item').eq(numberOfNextProduct + 1).data('price');
+    var nextProductTitle = $('.products__item').eq(numberOfNextProduct + 1).find('.products__title').text();
+    var nextProductDescription = $('.products__item').eq(numberOfNextProduct + 1).find('.products__description').text();
+    var photoNumber = $('.products__item').eq(numberOfNextProduct + 1).index();
+
+    $('.preview').show();
+    $('.preview__photo-item').attr('src', nextPhotoSrc);
+    $('.preview__product-fabric span').text(nextProductFabric);
+    $('.preview__product-size span').text(nextProductSize);
+    $('.preview__product-price span').text(nextProductPrice + '$');
+    $('.preview__product-title').text(nextProductTitle);
+    $('.preview__product-description-text').text(nextProductDescription);
+    $('.preview__content').data('product-number', photoNumber);
+};
+
+var showPrevPreview = function (element) {
+    var numberOfNextProduct = $(element).closest('.preview__content').data('product-number');
+    var nextPhotoSrc = $('.products__item').eq(numberOfNextProduct - 1).find('.products__photo').attr('src');
+    var nextProductFabric = $('.products__item').eq(numberOfNextProduct - 1).data('fabric');
+    var nextProductSize = $('.products__item').eq(numberOfNextProduct - 1).data('size');
+    var nextProductPrice = $('.products__item').eq(numberOfNextProduct - 1).data('price');
+    var nextProductTitle = $('.products__item').eq(numberOfNextProduct - 1).find('.products__title').text();
+    var nextProductDescription = $('.products__item').eq(numberOfNextProduct - 1).find('.products__description').text();
+    var photoNumber = $('.products__item').eq(numberOfNextProduct - 1).index();
+
+    $('.preview').show();
+    $('.preview__photo-item').attr('src', nextPhotoSrc);
+    $('.preview__product-fabric span').text(nextProductFabric);
+    $('.preview__product-size span').text(nextProductSize);
+    $('.preview__product-price span').text(nextProductPrice + '$');
+    $('.preview__product-title').text(nextProductTitle);
+    $('.preview__product-description-text').text(nextProductDescription);
+    $('.preview__content').data('product-number', photoNumber);
+};
+
+$('.preview__next-button').on('click', function () {
+    showNextPreview(this);
+});
+
+$('.preview__prev-button').on('click', function () {
+   showPrevPreview(this);
+});
 
 
