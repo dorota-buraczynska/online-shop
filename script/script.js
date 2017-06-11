11a1f5ca0d6a004bdf97620858a9ce54d2e3a370
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-06-11 21:30:00 +0200

    blog from entries.json, node_modules in gitignore

--- script/script.js
+++ script/script.js
@@ -424,3 +424,48 @@ $('.filter__button').on('click', function () {
 });
 
 
+// blog
+var renderEntries = function (entries) {
+    var blogWrapper = $('.blog__wrapper');
+    blogWrapper.empty();
+    for (var i = 0; i < entries.length; i++) {
+        var entry = $('<div>', {
+            'class': 'blog__entry',
+            'data-id': entries[i].id
+        });
+        var entryPhoto = $('<img>', {
+            'class': "blog__entry-photo",
+            'src': entries[i].img_src
+        });
+        var entriesWrapper = $('<div>', {
+            'class': 'blog__entry-wrapper'
+        });
+        var entryTitle = $('<div>', {'class': 'blog__entry-title'}).text(entries[i].title);
+        var entryText = $('<div>', {'class': 'blog__entry-text'}).text(entries[i].text);
+        var entryButton = $('<button>', {'class': 'blog__button'}).text('read more');
+
+        blogWrapper.append(entry);
+        entry
+            .append(entryPhoto)
+            .append(entriesWrapper)
+            .append(entryButton);
+        entriesWrapper
+            .append(entryTitle)
+            .append(entryText);
+    }
+};
+
+var loadEntries = function () {
+    $.ajax({
+        url: 'db/entries.json',
+        method: 'GET',
+        dataType: 'json'
+    }).done(function (response) {
+        renderEntries(response.entries);
+        console.log(response.entries);
+    }).fail(function (error) {
+        console.log(error);
+    })
+};
+
+loadEntries();
