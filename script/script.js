90d3b734d5415e116e17aef7d14c47b1b6d3623b
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-06-11 21:47:23 +0200

    add renderCart function

--- script/script.js
+++ script/script.js
@@ -185,27 +185,22 @@ $(window).on('resize', function () {
 //cart
 var cartArray = [];
 var priceArray = [];
-var indexArray = [];
+var totalSum = 0;
 
 var addProductToCart = function (productIndex) {
     var product = {
         path: $('.products__item').eq(productIndex).find('.products__photo').attr('src'),
         price: $('.products__item').eq(productIndex).data('price'),
         size: $('.products__item').eq(productIndex).data('size'),
-        index: $('.products__item').eq(productIndex).index()
+        index: $('.products__item').eq(productIndex).data('id')
     };
     cartArray.push(product);
     priceArray.push(product.price);
-    indexArray.push(product.index);
 };
 
-
-$('.nav__basket').on('click', function () {
-    $('.shopping-list__wrapper .shopping-list__product-wrapper').remove();
-
+var renderCart = function () {
     var buttonsWrapper = ('<div class="shopping-list__buttons-wrapper"><div class="shopping-list__plus-button">&#43;</div><div class="shopping-list__minus-button">&#45;</div></div>');
     var deleteButton = ('<button class="shopping-list__delete-button">delete</button>');
-    var totalSum = 0;
 
     for (var i = 0; i < cartArray.length; i++) {
         var productPhoto = ('<img class="shopping-list__product-photo" src="' + cartArray[i].path + '">');
@@ -217,24 +212,31 @@ $('.nav__basket').on('click', function () {
             .prepend(' <div class="shopping-list__product-wrapper">' + productPhoto + productSize + productAmount + productPrice + deleteButton + '</div>');
 
     }
+};
+
+var deleteProductFromBasket = function (element) {
+    var price = $(element).closest('.shopping-list__product-wrapper').find('.shopping-list__product-price').text();
+    var number = parseInt(price.slice(1, 4));
+    totalSum -= number;
+    $('.shopping-list__product-total-price span').text('$' + totalSum);
+    $(this).closest('.shopping-list__product-wrapper').remove();
+    console.log(price);
+};
+
+$('.nav__basket').on('click', function (event) {
+    event.preventDefault();
+    $('.shopping-list__wrapper .shopping-list__product-wrapper').remove();
+    renderCart();
 
     for (var i = 0; i < priceArray.length; i++) {
         totalSum += priceArray[i];
         $('.shopping-list__product-total-price span').text('$' + totalSum);
     }
-
-    //delete product from shopping list
-    $('.shopping-list__delete-button').on('click', function () {
-        var allPrice = $(this).closest('.shopping-list__product-wrapper').find('.shopping-list__product-price').text();
-        var number = parseInt(allPrice.slice(1, 4));
-        totalSum -= number;
-        $('.shopping-list__product-total-price span').text('$' + totalSum);
-        $(this).closest('.shopping-list__product-wrapper').remove();
-    });
-
-
 });
 
+$('.shopping-list__wrapper').on('click', '.shopping-list__delete-button', function () {
+    deleteProductFromBasket(this);
+});
 
 $('.preview__basket-button').on('click', function () {
     var productIndex = $(this).closest('.preview__content').data('product-number');
