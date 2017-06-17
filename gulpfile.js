a7d67a7ba0a97f0b4647d2ded49e5252ca4def46
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-06-17 11:56:12 +0200

    add livereload to gulp add placeholders for buttons change transition duration, and add it to variable

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+
+function onError(error) {
+    console.log(error);
+    this.emit('end');
+}
 
 gulp.task('sass', function () {
     return gulp.src('sass/main.scss')
         .pipe(sass({errLogToConsole: true}))
+        .on('error', onError)
         .pipe(gulp.dest('css'))
+        .pipe(livereload())
 });
 
 gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('sass/**/*.scss', ['sass']);
     gulp.watch('script/*.js', ['js']);
 });
@@ -20,4 +29,5 @@ gulp.task('js', function () {
         .pipe(concat('main.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('js'))
+        .pipe(livereload())
 });
